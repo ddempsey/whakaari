1bdfc94e87be904cdf40ecfeddd2dd70552b496b
David Dempsey <ddempsey786@gmail.com>
2020-10-08 08:49:20 +1300

    start geonet data pull

---
+++ scripts/pull_geonet_data.py
@@ -0,0 +1,10 @@
+import os, sys, traceback, smtplib, ssl, yagmail, shutil, argparse
+sys.path.insert(0, os.path.abspath('..'))
+from whakaari import TremorData, ForecastModel, to_nztimezone, datetimeify
+from datetime import timedelta, datetime
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
