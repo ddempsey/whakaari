2f05e9f21445a85c5af1638882ace86792f2f29f
Stephen Lee Orenia <stephen.orenia@gmail.com>
2021-02-19 16:10:37 +1300

    Added model calibration with sigmoid function
    - sig_params in forecast, hires_forecast to represent a and b
    - sigmoid() function
      - Return the calibrated probability by applying the sigmoid function to the model consensus.
    - plot_forecast_calibrated for probability version of plot_forecast
      - changes to y limits and threshold
    - similarly plot_hires_forecast_calibrated for plot_hires_forecast
      - important changes to compute_CI implementation

--- whakaari/__init__.py
+++ whakaari/__init__.py
@@ -10,6 +10,7 @@ import numpy as np
 from datetime import datetime, timedelta, date
 from copy import copy
 from matplotlib import pyplot as plt
+import matplotlib.ticker as mtick
 from inspect import getfile, currentframe
 from glob import glob
 import pandas as pd
@@ -20,6 +21,7 @@ from time import time
 from scipy.integrate import cumtrapz
 from scipy.signal import stft
 from scipy.optimize import curve_fit
+from scipy.special import expit
 from corner import corner
 from functools import partial
 from fnmatch import fnmatch
@@ -1103,7 +1105,7 @@ class ForecastModel(object):
         del fM
         gc.collect()
         self._collect_features()
-    def forecast(self, ti=None, tf=None, recalculate=False, use_model=None, n_jobs=None):
+    def forecast(self, ti=None, tf=None, recalculate=False, use_model=None, n_jobs=None, sig_params=None):
         """ Use classifier models to forecast eruption likelihood.
 
             Parameters:
@@ -1119,6 +1121,8 @@ class ForecastModel(object):
                 Optionally pass path to pre-trained model directory in 'models'.
             n_jobs : int
                 Number of cores to use.
+            sig_params : None or dict/list/tuple
+                If given, apply sigmoid parameters to predictions. These are calculated using calibration script
 
             Returns:
             --------
@@ -1211,6 +1215,10 @@ class ForecastModel(object):
         ys = pd.concat(ys, axis=1, sort=False)
         consensus = np.mean([ys[col].values for col in ys.columns if 'pred' in col], axis=0)
         forecast = pd.DataFrame(consensus, columns=['consensus'], index=ys.index)
+
+        if sig_params is not None:
+            self.sig_params = sig_params
+            forecast['probability'] = forecast['consensus'].apply(sigmoid, sig_params=sig_params)
         # forecast.to_csv('{:s}/consensus.csv'.format(self.preddir), index=True, index_label='time')
         save_dataframe(forecast, '{:s}/consensus.{:s}'.format(self.preddir,self.savefile_type), index=True, index_label='time')
         
@@ -1221,7 +1229,7 @@ class ForecastModel(object):
 
         return forecast
     def hires_forecast(self, ti, tf, recalculate=True, save=None, root=None, nztimezone=False, 
-        n_jobs=None, threshold=0.888, alt_rsam=None, xlim=None):
+        n_jobs=None, threshold=0.888, alt_rsam=None, xlim=None, sig_params=None):
         """ Construct forecast at resolution of data.
 
             Parameters:
@@ -1241,6 +1249,8 @@ class ForecastModel(object):
                 Flag to plot forecast using NZ time zone instead of UTC.            
             n_jobs : int
                 CPUs to use when forecasting in parallel.
+            sig_params : None or dict/list/tuple
+                If given, apply sigmoid parameters to predictions. These are calculated using calibration script
             Notes:
             ------
             Requires model to have already been trained.
@@ -1265,10 +1275,14 @@ class ForecastModel(object):
         _fm._extract_features(ti, tf)
 
         # predict on hires features
-        ys = _fm.forecast(ti, tf, recalculate, use_model=self.modeldir, n_jobs=n_jobs)
+        ys = _fm.forecast(ti, tf, recalculate, use_model=self.modeldir, n_jobs=n_jobs, sig_params=sig_params)
         
         if save is not None:
             self._plot_hires_forecast(ys, save, threshold, nztimezone=nztimezone, alt_rsam=alt_rsam, xlim=xlim)
+            if sig_params is not None: # Assume user wants both hires forecasts as model output and probability
+                save=save.split('.png')[0]
+                c_save = f'{save}_calibrated.png'
+                self._plot_hires_forecast_calibrated(ys, c_save, sigmoid(threshold,sig_params), nztimezone=nztimezone, alt_rsam=alt_rsam, xlim=xlim)
 
         return ys
     # plotting methods
@@ -1310,7 +1324,7 @@ class ForecastModel(object):
         t = ys.index
 
         # average individual model responses
-        ys = np.mean(np.array([ys[col] for col in ys.columns]), axis=0)
+        ys = np.mean(np.array([ys[col] for col in ys.columns if 'probability' not in col]), axis=0)
         for i,ax in enumerate(axs):
 
             ax.set_ylim([-0.05, 1.05])
@@ -1399,7 +1413,7 @@ class ForecastModel(object):
                 alt_trsam = to_nztimezone(alt_trsam)
         else:
             ax2.set_xlabel('UTC')
-        y = np.mean(np.array([ys[col] for col in ys.columns]), axis=0)
+        y = np.mean(np.array([ys[col] for col in ys.columns if 'probability' not in col]), axis=0)
         
         ts = [t[-1], trsam[-1]]
         if alt_rsam is not None: ts.append(alt_trsam[-1])
@@ -1457,6 +1471,186 @@ class ForecastModel(object):
         ax1.set_xticks(xts)
         ax1.set_xticklabels(lxts)
         ax1.text(0.025, 0.95, t0.strftime('%Y'), size = 12, ha = 'left', 
+            va = 'top', transform=ax1.transAxes)
+
+        plt.savefig(save, dpi=400)
+        plt.close(f)
+    def plot_forecast_calibrated(self, ys, threshold=0.025, save=None, xlim=['2019-12-01','2020-02-01']):
+        """ Plot model forecast.
+
+            Parameters:
+            -----------
+            ys : pandas.DataFrame
+                Model forecast returned by ForecastModel.forecast.
+            threshold : float
+                Probability Threshold to declare alert.
+            save : str
+                File name to save figure.
+            local_time : bool
+                If True, switches plotting to local time (default is UTC).
+
+        """
+        makedir(self.plotdir)
+        if save is None:
+            save = '{:s}/forecast.png'.format(self.plotdir)
+        # set up figures and axes
+        f = plt.figure(figsize=(24,15))
+        N = 10
+        dy1,dy2 = 0.05, 0.05
+        dy3 = (1.-dy1-(N//2)*dy2)/(N//2)
+        dx1,dx2 = 0.37,0.04
+        axs = [plt.axes([0.10+(1-i//(N/2))*(dx1+dx2), dy1+(i%(N/2))*(dy2+dy3), dx1, dy3]) for i in range(N)][::-1]
+
+        for i,ax in enumerate(axs[:-1]):
+            ti,tf = [datetime.strptime('{:d}-01-01 00:00:00'.format(2011+i), '%Y-%m-%d %H:%M:%S'),
+                datetime.strptime('{:d}-01-01 00:00:00'.format(2012+i), '%Y-%m-%d %H:%M:%S')]
+            ax.set_xlim([ti,tf])
+            ax.text(0.01,0.95,'{:4d}'.format(2011+i), transform=ax.transAxes, va='top', ha='left', size=16)
+
+        ti,tf = [datetimeify(x) for x in xlim]
+        axs[-1].set_xlim([ti, tf])
+
+        # model forecast is generated for the END of each data window
+        t = ys.index
+
+        # calibrated probabilities
+        ps = np.array(ys['probability'])
+
+        for i,ax in enumerate(axs):
+
+            ax.set_ylim([-0.005, 0.06]) # ylimit for probabilities
+            ax.set_yticks([0, 0.02, 0.04, 0.06])
+            ax.yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1)) # Formatting ticks as percentages
+            if i//(N/2) == 0:
+                ax.set_ylabel('eruption probability')
+            else:
+                ax.set_yticklabels([])
+
+            # shade training data
+            ax.fill_between([self.ti_train, self.tf_train],[-0.05,-0.05],[1.05,1.05], color=[0.85,1,0.85], zorder=1, label='training data')
+            for exclude_date_range in self.exclude_dates:
+                t0,t1 = [datetimeify(dt) for dt in exclude_date_range]
+                ax.fill_between([t0, t1],[-0.05,-0.05],[1.05,1.05], color=[1,1,1], zorder=2)
+
+            # consensus threshold
+            ax.axhline(threshold, color='k', linestyle=':', label='alert threshold', zorder=4)
+
+            # modelled alert
+            ax.plot(t, ps, 'c-', label='modelled alert', zorder=4)
+
+            # eruptions
+            for te in self.data.tes:
+                ax.axvline(te, color='k', linestyle='-', zorder=5)
+            ax.axvline(te, color='k', linestyle='-', label='eruption')
+
+        for tii,pi in zip(t, ps):
+            if pi > threshold:
+                i = (tii.year-2011)
+                axs[i].fill_between([tii, tii+self.dtf], [0,0], [1,1], color='y', zorder=3)
+                j = (tii+self.dtf).year - 2011
+                if j != i:
+                    axs[j].fill_between([tii, tii+self.dtf], [0,0], [1,1], color='y', zorder=3)
+
+                if tii > ti:
+                    axs[-1].fill_between([tii, tii+self.dtf], [0,0], [1,1], color='y', zorder=3)
+
+        for ax in axs:
+            ax.fill_between([], [], [], color='y', label='eruption forecast')
+        axs[-1].legend()
+
+        plt.savefig(save, dpi=400)
+        plt.close(f)
+    def _plot_hires_forecast_calibrated(self, ys, save, threshold=0.025, nztimezone=False, alt_rsam=None, xlim=None):
+        """ Probability version of Plot model hires version of model forecast (single axes).
+
+            Parameters:
+            -----------
+            ys : pandas.DataFrame
+                Model forecast returned by ForecastModel.forecast.
+            threshold : float
+                Probability Threshold to declare alert.
+            save : str
+                File name to save figure.
+        """
+        makedir(self.plotdir)
+        # set up figures and axes
+        f = plt.figure(figsize=(8,8))
+        ax1 = plt.axes([0.1, 0.55, 0.8, 0.4])
+        ax2 = plt.axes([0.1, 0.08, 0.8, 0.4])
+        t = pd.to_datetime(ys.index.values)
+        rsam = self.data.get_data(t[0], t[-1])['rsam']
+        trsam = rsam.index
+        if alt_rsam is not None:
+            alt_trsam = alt_rsam.index
+        if nztimezone:
+            t = to_nztimezone(t)
+            trsam = to_nztimezone(trsam)
+            ax2.set_xlabel('Local time')
+            if alt_rsam is not None:
+                alt_trsam = to_nztimezone(alt_trsam)
+        else:
+            ax2.set_xlabel('UTC')
+        p = np.array(ys['probability']) # Make sure using probability for plots
+
+        ts = [t[-1], trsam[-1]]
+        if alt_rsam is not None: ts.append(alt_trsam[-1])
+        tmax = np.max(ts)
+        ax2.set_xlim([tmax-timedelta(days=7), tmax])
+        ax1.set_xlim([t[0], tmax])
+        ax1.set_title('Whakaari Eruption Forecast Probabilities')
+        for ax in [ax1,ax2]:
+            ax.set_ylim([-0.005, 0.06]) # ylimit for probabilities
+            ax.set_yticks([0, 0.02, 0.04, 0.06])
+            ax.yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1)) # Formatting ticks as percentages
+            ax.set_ylabel('probability')
+
+            # consensus threshold
+            ax.axhline(threshold, color='k', linestyle=':', label='alert threshold', zorder=4)
+
+            # modelled alert
+            ax.plot(t, p, 'c-', label='probability', zorder=4, lw=0.75)
+            y = np.mean(np.array([ys[col] for col in ys.columns if 'probability' not in col]), axis=0)
+            # convert regular model consensus confidence interval to probability space
+            ci = self.compute_CI(y)
+            ax.fill_between(t, sigmoid(y-ci, self.sig_params), sigmoid(y+ci, self.sig_params), color='c', zorder=5, alpha=0.3)
+            ax_ = ax.twinx()
+            ax_.set_ylabel('RSAM [$\mu$m s$^{-1}$]')
+            ax_.set_ylim([0,5])
+            ax_.set_xlim(ax.get_xlim())
+            if alt_rsam is not None:
+                ax_.plot(alt_trsam, alt_rsam.values*1.e-3, '-', color=[0.5,0.5,0.5], lw=0.75)
+            ax_.plot(trsam, rsam.values*1.e-3, 'k-', lw=0.75)
+
+            for tii,pi in zip(t, p):
+                if pi > threshold:
+                    ax.fill_between([tii, tii+self.dtf], [0,0], [100,100], color='y', zorder=3)
+
+            ax.fill_between([], [], [], color='y', label='eruption forecast')
+            ax.plot([],[],'k-', lw=0.75, label='RSAM')
+            if alt_rsam is not None:
+                ax.plot([],[],'-', color=[0.5,0.5,0.5], lw=0.75, label='RSAM (WSRZ-scaled)')
+        ax1.legend(loc=1, ncol=2)
+        if xlim is not None: 
+            ax2.set_xlim(xlim)
+            tmax = xlim[-1] 
+        tf = tmax 
+        t0 = tf.replace(hour=0, minute=0, second=0)
+        xts = [t0 - timedelta(days=i) for i in range(7)][::-1]
+        lxts = [xt.strftime('%d %b') for xt in xts]
+        ax2.set_xticks(xts)
+        ax2.set_xticklabels(lxts)
+        tfi  = self.data.tf
+        if nztimezone:
+            tfi = to_nztimezone([tfi])[0]
+        ax2.text(0.025, 0.95, 'model last updated {:s}'.format(tfi.strftime('%H:%M, %d %b %Y')), size = 12, ha = 'left',
+            va = 'top', transform=ax2.transAxes)
+
+        t0 = datetimeify('2020-01-01')
+        xts = [t0.replace(month=i) for i in range(1, tf.month+1)]
+        lxts = [xt.strftime('%d %b') for xt in xts]
+        ax1.set_xticks(xts)
+        ax1.set_xticklabels(lxts)
+        ax1.text(0.025, 0.95, t0.strftime('%Y'), size = 12, ha = 'left',
             va = 'top', transform=ax1.transAxes)
 
         plt.savefig(save, dpi=400)
@@ -2021,3 +2215,31 @@ def to_nztimezone(t):
     utctz = tz.gettz('UTC')
     nztz = tz.gettz('Pacific/Auckland')
     return [ti.replace(tzinfo=utctz).astimezone(nztz) for ti in pd.to_datetime(t)]
+
+
+def sigmoid(consensus, sig_params):
+    ''' Return the calibrated probability by applying the sigmoid function to the model consensus.
+
+        Parameters:
+        -----------
+        consensus : column of pd.DataFrame
+            The model consensus
+        sig_params : None or dict/list/tuple
+            Sigmoid parameters - {'a': a, 'b':b} or (a,b) or [a,b]
+
+        Returns:
+        --------
+        sigmoid : column of pd.DataFrame
+            Probabilities after applying sigmoid parameters to modeel consensus.
+
+        Notes:
+        ------
+        Helper function to apply a,b to set of predictions (see pd.DataFrame.apply())
+    '''
+    if isinstance(sig_params, dict):
+        a = sig_params['a']
+        b = sig_params['b']
+    elif isinstance(sig_params, (list,tuple)):
+        a = sig_params[0]
+        b = sig_params[1]
+    return expit(-(a * consensus + b))
